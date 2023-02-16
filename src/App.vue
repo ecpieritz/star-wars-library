755913886dc9f8288d130ef96a0796452c8ab750
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-02-16 10:56:53 -0300

    Added header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,45 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/people">People</router-link> |
-    <router-link to="/films">Films</router-link> |
-    <router-link to="/species">Species</router-link> |
-    <router-link to="/planets">Planets</router-link>
-  </nav>
+  <Header />
   <router-view/>
 </template>
 
+<script>
+import Header from './components/Header.vue'
+export default {
+  name: 'App',
+  components: {Header}
+}
+</script>
+
 <style lang="scss">
+@import './scss/main.scss';
+
+body{
+  background-color: $sw-black;
+  font-family: 'Noto Sans JP', sans-serif;
+  color: $sw-white;
+}
+
+.sw-header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 120px;
+  &__logo{
+    width: 200px;
+  }
+  &__nav{
+    display: flex;
+    gap: 20px;
+    color: $sw-white;
+    a{
+      color: $sw-white;
+      text-decoration: none;
+      &:hover{
+        color: $sw-red;
+        text-decoration: none;
+      }
+    }
+  }
+}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="sw-header">
+    <img class="sw-header__logo" src='../assets/imgs/sw-logo.png' alt="Star Wars logo">
+
+    <nav class="sw-header__nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/people">People</router-link>
+      <router-link to="/films">Films</router-link>
+      <router-link to="/species">Species</router-link>
+      <router-link to="/planets">Planets</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+  props: {}
+}
+</script>
+
+<style scoped lang="scss">
+</style>
