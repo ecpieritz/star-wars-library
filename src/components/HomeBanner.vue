e073296706b7c41da92c10f582579c5734378029
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-02-16 11:45:47 -0300

    Added home banner

---
+++ src/components/HomeBanner.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="sw-home-banner">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="../assets/imgs/sw-banner__darkside.jpg" class="d-block w-100" alt="Darkside">
+        </div>
+        <div class="carousel-item">
+          <img src="../assets/imgs/sw-banner__jedi.jpg" class="d-block w-100" alt="Jedi">
+        </div>
+        <div class="carousel-item">
+          <img src="../assets/imgs/sw-banner__rebels.jpg" class="d-block w-100" alt="Rebels">
+        </div>
+      </div>
+    </div>
+</div>
+</template>
+  
+<script>
+export default {
+  name: 'HomeBanner',
+  props: {}
+}
+</script>
+  
+<style scoped lang="scss"></style>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="sw-home">
+    <HomeBanner />
     <h1>Home</h1>
   </div>
 </template>
 
 <script>
+import HomeBanner from '@/components/HomeBanner.vue'
 
 export default {
   name: 'HomeView',
-  components: {
+  components: { HomeBanner
   }
 }
 </script>
