6573632274e00f4ee0f09cd6348d6e3bf83833d3
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-02-18 09:42:32 -0300

    Added Species API

--- src/views/SpeciesView.vue
+++ src/views/SpeciesView.vue
@@ -1,6 +1,36 @@
 <template>
-    <div class="sw-species">
-      <h1>Species</h1>
-    </div>
-  </template>
+  <div class="sw-species">
+    <h1>Species</h1>
+  </div>
+</template>
   
+<script>
+import api from '@/services/api';
+import { ref, onMounted } from 'vue';
+
+export default {
+  name: 'FilmsView',
+  setup() {
+    const films = ref([])
+
+    const fetchFilms = async () => api.get('/films?limit=20').then((response) => films.value = response.data.results)
+
+    onMounted(fetchFilms)
+
+    return { films }
+  }
+}
+</script>
+  
+<style scoped lang="scss">
+@import '@/scss/main.scss';
+
+.sw-films {
+  margin-top: 14vh;
+  color: $sw-white;
+
+  @media(max-width:450px) {
+    margin-top: 19vh;
+  }
+}
+</style>
