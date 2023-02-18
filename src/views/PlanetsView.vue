2ec962e122c36d927c05d78040b237f434693da2
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-02-18 09:41:56 -0300

    Added Planets API

--- src/views/PlanetsView.vue
+++ src/views/PlanetsView.vue
@@ -4,3 +4,32 @@
     </div>
   </template>
   
+  <script>
+  import api from '@/services/api';
+  import {ref, onMounted} from 'vue';
+  
+  export default {
+    name: 'planetsView',
+    setup(){
+      const planets = ref([])
+  
+      const fetchPlanets = async ()=> api.get('/planets?limit=20').then((response)=> planets.value = response.data.results)
+  
+      onMounted(fetchPlanets)
+  
+      return{ planets}
+    }
+  }
+  </script>
+  
+  <style scoped lang="scss">
+  @import '@/scss/main.scss';
+  
+  .sw-planets{
+    margin-top: 14vh;
+    color: $sw-white;
+    @media(max-width:450px){
+      margin-top: 19vh;
+    }
+  }
+  </style>
