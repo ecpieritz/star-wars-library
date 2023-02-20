c2af718861125110e594f1ad197970bbe234b08c
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-02-20 15:44:50 -0300

    Added final twiks

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <HomeBanner />
     <section class="container-fluid">
       <h1>A long time ago in a galaxy far, far away...</h1>
-  
+
       <div class="row">
         <div class="col-lg-8">
           <p>Star Wars is an American epic space opera multimedia franchise created by George Lucas, which began with the eponymous 1977 film and quickly became a worldwide pop culture phenomenon. The franchise has been expanded into various films and other media, including television series, video games, novels, comic books, theme park attractions, and themed areas, comprising an all-encompassing fictional universe. Star Wars is one of the highest-grossing media franchises of all time.</p>
