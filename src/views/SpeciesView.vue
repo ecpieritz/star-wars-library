c972c1b96224ff9f0db08736efa3b50f680bf848
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-02-20 15:29:35 -0300

    Added Species text

--- src/views/SpeciesView.vue
+++ src/views/SpeciesView.vue
@@ -3,6 +3,8 @@
     <div class="container-fluid">
       <h1>Species</h1>
 
+      <p>Below is some information about:</p>
+
       <div class="row">
         <section class="col-lg-7 sw-table">
           <table>
@@ -27,6 +29,21 @@
           <SpeciesChart />
         </aside>
 
+        <section class="sw-content__text">
+          <div class="row">
+            <div class="col-lg-8">
+              <h2>Some famous species from the saga:</h2>
+              <h4>Wookie</h4>
+              <p>Wookiees are fictional humanoid aliens in the Star Wars universe, native to the forest planet Kashyyyk. They are distinguished from humans by their gigantism, hirsutism, and physical strength. The most prominent Wookiee is Chewbacca, co-pilot of the Millennium Falcon alongside his best friend Han Solo.</p>
+
+              <h4>Sith</h4>
+              <p>The Sith (also known as Red Sith during the time of the Old Sith Empire and Sith Purebloods during the time of the resurgent Sith Empire) were the native species of the planet Korriban. They had red skin and tentacle "beards", and had a natural talent for mastering the Force as well as their magic and alchemy. Naturally cruel and manipulative, the Sith spread throughout the outer rim in the early days of the Old Republic, eventually centering their empire on Korriban. Some time after the Great Schism, several refugee Dark Jedi settled on the planet.</p>
+            </div>
+            <div class="col-lg-4">
+              <img src="@/assets/imgs/sw-species__content.jpg" alt="Star Wars poster">
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   </main>
