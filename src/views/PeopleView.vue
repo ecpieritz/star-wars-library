b6d30b33d143fcd17735fe575d88abf169d67990
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2023-02-20 15:28:42 -0300

    Added People text

--- src/views/PeopleView.vue
+++ src/views/PeopleView.vue
@@ -3,6 +3,8 @@
     <div class="container-fluid">
       <h1>People</h1>
 
+      <p>Below is some information about:</p>
+
       <div class="row">
         <section class="col-lg-7 sw-table">
           <table>
@@ -27,6 +29,21 @@
           <PeopleChart />
         </aside>
 
+        <section class="sw-content__text">
+          <div class="row">
+            <div class="col-lg-8">
+              <h2>Some famous people from the saga:</h2>
+              <h4>Luke Skywalker</h4>
+              <p>Luke Skywalker is a fictional character and the protagonist of the original film trilogy of the Star Wars franchise created by George Lucas. Portrayed by Mark Hamill, Luke first appeared in Star Wars (1977), and he returned in The Empire Strikes Back (1980) and Return of the Jedi (1983). Over three decades later, Hamill returned as Luke in the Star Wars sequel trilogy, cameoing in The Force Awakens (2015) before playing a major role in The Last Jedi (2017) and The Rise of Skywalker (2019). He later played a digitally de-aged version of the character in the Disney+ series The Mandalorian, appearing in the second-season finale, which premiered in 2020, and The Book of Boba Fett, in the sixth episode, released in 2022.</p>
+              
+              <h4>Darth Vader</h4>
+              <p>Darth Vader is a fictional character in the Star Wars franchise. The character is the central antagonist of the original trilogy and, as Anakin Skywalker, is one of the main protagonists in the prequel trilogy. Star Wars creator George Lucas has collectively referred to the first six episodic films of the franchise as "the tragedy of Darth Vader". He has become one of the most iconic villains in popular culture, and has been listed among the greatest villains and fictional characters ever. His masked face is one of the most iconic character designs of all time.</p>
+            </div>
+            <div class="col-lg-4">
+              <img src="@/assets/imgs/sw-people__content.jpg" alt="Star Wars poster">
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   </main>
